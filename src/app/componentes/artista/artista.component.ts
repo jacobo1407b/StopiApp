6c4e8f8f7728e2fa16375f6d7693371d3bbdac57
jacobo1407b <jacobohernandezmendieta7@gmail.com>
2020-02-10 22:26:42 -0600

    tokens

---
+++ src/app/componentes/artista/artista.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SpotifyService } from 'src/app/services/spotify.service';
+
+@Component({
+  selector: 'app-artista',
+  templateUrl: './artista.component.html',
+  styles: []
+})
+export class ArtistaComponent {
+  artista: any ={};
+
+  constructor(private router:ActivatedRoute,
+                private spotify: SpotifyService) { 
+this.router.params.subscribe(params => {
+  this.getArtista(params['id']);
+});
+
+  }
+  getArtista(id : string){
+ this.spotify.getArtista(id)
+     .subscribe(artista => {
+        this.artista = artista;
+     });
+  }
+
+  
+
+}
